bc266f94d8a3c91a2900174d585b9b5f1f6c9dd1
danton <danton.cortes@gmail.com>
2023-09-22 14:29:00 -0300

    Reporte semanal vista

--- ManejoPresupuesto/Servicios/ServicioReportes.cs
+++ ManejoPresupuesto/Servicios/ServicioReportes.cs
@@ -5,6 +5,7 @@ namespace ManejoPresupuesto.Servicios
 
     public interface IServicioReportes
     {
+        Task<IEnumerable<ResultadoObtenerPorSemana>> ObtenerReporteSemanal(int usuarioId, int mes, int año, dynamic ViewBag);
         Task<ReporteTransaccionesDetalladas> ObtenerReporteTransaccionesDetalladas(int usuarioId, int mes, int año, dynamic ViewBag);
         Task<ReporteTransaccionesDetalladas> ObtenerReporteTransaccionesDetalladasPorCuenta(int usuarioId, int cuentaId, int mes, int año, dynamic ViewBag);
     }
