2ab37f702cd081afb2ce7a34aede20e6c96191d3
danton <danton.cortes@gmail.com>
2023-11-20 11:09:44 -0300

    Configurando el sistema de usuarios

---
+++ ManejoPresupuesto/Servicios/RepositorioUsuarios.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioUsuarios
+    {
+        Task<Usuario> BuscarUsuarioPorEmail(string emailNormalizado);
+        Task<int> CrearUsuario(Usuario usuario);
+    }
+
+    public class RepositorioUsuarios: IRepositorioUsuarios
+    {
+        private readonly string connectionString;
+        public RepositorioUsuarios(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<int> CrearUsuario(Usuario usuario)
+        {
+            //usuario.Emailnormalizado = usuario.Email.ToUpper();
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"Insert into Usuarios (Email, EmailNormalizado, PasswordHash) 
+                                                                            Values (@Email, @EmailNormalizado, @PasswordHash)", usuario);
+
+            return id;
+        }
+
+        public async Task<Usuario> BuscarUsuarioPorEmail(string emailNormalizado)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QuerySingleOrDefaultAsync<Usuario>("Select * from Usuarios Where EmailNormalizado = @emailNormalizado", new { emailNormalizado });
+        }
+    }
+}
